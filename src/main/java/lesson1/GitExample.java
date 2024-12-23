bc7292312d18595aa42fdfa173e0f87d8c4ef162
Kudaev <s.kudaev@andersenlab.com>
2024-12-23 18:00:32 +0300

    Added new line

--- src/main/java/lesson1/GitExample.java
+++ src/main/java/lesson1/GitExample.java
@@ -3,5 +3,6 @@ package main.java.lesson1;
 public class GitExample {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("New changes");
     }
 }
