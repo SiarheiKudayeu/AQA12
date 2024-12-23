4a5db5a0149fc2ac024f8b9a151827a347bc37de
SiarheiKudayeu <102074437+SiarheiKudayeu@users.noreply.github.com>
2024-12-23 18:03:16 +0300

    Update GitExample.java

--- src/main/java/lesson1/GitExample.java
+++ src/main/java/lesson1/GitExample.java
@@ -4,5 +4,6 @@ public class GitExample {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("New changes");
+        System.out.println("My friend's commit");
     }
 }
